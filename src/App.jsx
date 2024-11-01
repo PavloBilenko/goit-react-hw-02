6269db63d6a83b7a2f3966cf414a34c9ce1b3b35
PavloBilenko <bilenko.pavlo@gmail.com>
2024-11-01 16:48:38 +0100

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import "modern-normalize";
 import Description from "./components/Description/Description";
@@ -8,11 +8,14 @@ import Feedback from "./components/Feedback/Feedback";
 // console.log(profileData);
 
 function App() {
-  const [voteData, setVoteData] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [voteData, setVoteData] = useState(() => {
+    const savedData = localStorage.getItem("voteData");
+    return savedData ? JSON.parse(savedData) : { good: 0, neutral: 0, bad: 0 };
   });
+
+  useEffect(() => {
+    localStorage.setItem("voteData", JSON.stringify(voteData));
+  }, [voteData]);
 
   const hasFeedback =
     voteData.good > 0 || voteData.neutral > 0 || voteData.bad > 0;
