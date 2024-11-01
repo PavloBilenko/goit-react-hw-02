80f6f559c9230cce9f2f1c20f2c0bd093995a04c
PavloBilenko <bilenko.pavlo@gmail.com>
2024-11-01 20:01:01 +0100

    -

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,27 @@ function App() {
   const totalFeedback = voteData.good + voteData.neutral + voteData.bad;
   const positivePercentage =
     totalFeedback > 0 ? (voteData.good / totalFeedback) * 100 : 0;
-
   const hasFeedback = totalFeedback > 0;
+
+  const handleFeedbackUpdate = (type) => {
+    setVoteData((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const handleReset = () => {
+    setVoteData({ good: 0, neutral: 0, bad: 0 });
+  };
 
   return (
     <div>
       <Description />
-      <Options voteData={voteData} setVoteData={setVoteData} />
+      <Options
+        totalFeedback={totalFeedback}
+        onFeedbackUpdate={handleFeedbackUpdate}
+        onReset={handleReset}
+      />
       {hasFeedback ? (
         <Feedback
           voteData={voteData}
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,58 +1,44 @@
-
-import React from "react";
+import PropTypes from "prop-types";
 import s from "./Options.module.css";
 
-const Options = ({ voteData, setVoteData }) => {
-  const handleClickByOption = (value) => {
-    setVoteData((prev) => ({
-      ...prev,
-      [value]: prev[value] + 1,
-    }));
-  };
-
-  const handleReset = () => {
-    setVoteData({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
-
+const Options = ({ totalFeedback, onFeedbackUpdate, onReset }) => {
   return (
     <div className={s.wrapper}>
       <ul className={s.list}>
         <li>
-          <button
-            onClick={() => handleClickByOption("good")}
-            className={s.button}
-          >
+          <button onClick={() => onFeedbackUpdate("good")} className={s.button}>
             Good
           </button>
         </li>
         <li>
           <button
-            onClick={() => handleClickByOption("neutral")}
+            onClick={() => onFeedbackUpdate("neutral")}
             className={s.button}
           >
             Neutral
           </button>
         </li>
         <li>
-          <button
-            onClick={() => handleClickByOption("bad")}
-            className={s.button}
-          >
+          <button onClick={() => onFeedbackUpdate("bad")} className={s.button}>
             Bad
           </button>
         </li>
-        <li>
-          <button onClick={handleReset} className={s.button}>
-            Reset
-          </button>
-        </li>
+        {totalFeedback > 0 && (
+          <li>
+            <button onClick={onReset} className={s.button}>
+              Reset
+            </button>
+          </li>
+        )}
       </ul>
     </div>
   );
+};
+
+Options.propTypes = {
+  totalFeedback: PropTypes.number.isRequired,
+  onFeedbackUpdate: PropTypes.func.isRequired,
+  onReset: PropTypes.func.isRequired,
 };
 
 export default Options;
